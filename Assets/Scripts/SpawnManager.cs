d648c3438332f8d6d25a4ad5faf9e983bb767fd5
David Ash <davidjash2@gmail.com>
2023-05-18 20:34:12 -0400

    converted game to 2d and added trippleshot power up

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private float _waitTime = 5;
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+    [SerializeField]
+    private GameObject _tripleShotPowerupPrefab;
+
+    private bool _stopSpawning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IEnumerator enemyCoroutine = SpawnEnemyRoutine();
+        StartCoroutine(enemyCoroutine);
+        IEnumerator powerUpCoroutine = SpawnPowerupRoutine();
+        StartCoroutine(powerUpCoroutine);
+    }
+
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+    }
+
+    IEnumerator SpawnEnemyRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            float randomX = Random.Range(-8f, 8f);
+            Vector3 posToSpawn = new Vector3(randomX, 7, 0);
+            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(_waitTime);
+        }
+    }
+    IEnumerator SpawnPowerupRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            float randomX = Random.Range(-8f, 8f);
+            Vector3 posToSpawn = new Vector3(randomX, 7, 0);
+            Instantiate(_tripleShotPowerupPrefab, posToSpawn, Quaternion.identity);
+            int randomWaitTime = Random.Range(3, 8);
+            yield return new WaitForSeconds(randomWaitTime);
+        }
+    }
+}
